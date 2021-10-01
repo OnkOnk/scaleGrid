b84d89665d787ddb521f50e6e43c6d84a68ef231
Daniel Rydén <Daniel.Ryden@etraveligroup.com>
2021-10-01 23:02:19 +0200

    test

--- gridScale.js
+++ gridScale.js
@@ -1188,7 +1188,7 @@ caclByXnY(x,y) {
           label: "Set the grid",
           callback: dlg => {
             const form = dlg.find('#compendium-create'),
-                  data = FormApplication.getData(form[0]);
+                  data = dlg.getData(form[0]);
                   //console.log(data);
                   //sgLayer.testVar3 = data;
                   sgLayer.caclByXnY(data.xNum,data.yNum);
